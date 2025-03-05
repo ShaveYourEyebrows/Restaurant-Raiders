015d3d7307ef76f509e99d209a72b3d1048f316b
Jacob Beck <jacobgrbeck@gmail.com>
2025-03-05 15:45:42 +0000

    Making things look nice

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,9 +5,24 @@ function Contact(){
     return(
         <>
             <Topbar />
-            <div>
+            <div className='numDiv'>
                 <h2 className='contactDetails'>Manager Contact Details:</h2>
-                <h4 className='phoneNumber'>Manager John: 087-457-4007</h4>
+                <ul className='phoneNumberList'>
+                    <li className='phoneNumberItem'>Manager John: 087-457-4007</li>
+                    <li className='phoneNumberItem'>Manager Kovacs: 087-461-1087</li>
+                    <li className='phoneNumberItem'>Manager Freddy: 087-983-1987</li>
+                    <li className='phoneNumberItem'>Manager Markus: 087-064-2009</li>
+                </ul>
+            </div>
+
+            <div className='noticeDiv'>
+                <h2 className='noticeHeader'>Notice to Employees</h2>
+                <p className='noticePara'>
+                    We have gotten reports of employees contacting
+                    managers after hours! Please refrain from contacting
+                    higher-ups after 5PM, including those on the night shift.
+                    Thank you - Manager Kovacs.
+                </p>
             </div>
         </>
     );
